1f1e6b7b10e545baceb192a7ced06eeac7f9f674
hyazhar <uddinmdazhar21@gmail.com>
2026-01-30 11:14:26 +0530

    Add Navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+let hamburger = document.getElementById("hamburger-menu");
+let menus = document.getElementById("menus");
+/* Toggle Menu */
+hamburger.addEventListener("click", () => {
+    menus.classList.toggle("active");
+});
+
