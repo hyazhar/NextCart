9745228c9c15d78f2e23ed7df51a5de2d409c6dc
hyazhar <uddinmdazhar21@gmail.com>
2026-02-06 11:02:39 +0530

    Added Image slider but not added functionality yet

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let hamburger = document.getElementById("hamburger-menu");
 let menus = document.getElementById("menus");
+
 /* Toggle Menu */
 hamburger.addEventListener("click", () => {
     menus.classList.toggle("active");
 });
+
 
