0d02beaf54208a9620c0ee3e6cb0bb5f02cb72c1
hyazhar <uddinmdazhar21@gmail.com>
2026-02-07 08:49:37 +0530

    Add image  Slider Animation

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 let hamburger = document.getElementById("hamburger-menu");
 let menus = document.getElementById("menus");
+const track = document.querySelector(".crousel-track");
+const slides = document.querySelectorAll(".crousel-item");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+console.log(track);
+let index = 0;
 
+function updateSlide() {
+  track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  index = (index + 1) % slides.length;
+  updateSlide();
+});
+
+prevBtn.addEventListener("click", () => {
+  index = (index - 1 + slides.length) % slides.length;
+  updateSlide();
+});
 /* Toggle Menu */
 hamburger.addEventListener("click", () => {
     menus.classList.toggle("active");
