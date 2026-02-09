d51d899613b58b09834a9f7d7de13409f5db3652
hyazhar <uddinmdazhar21@gmail.com>
2026-02-09 07:48:01 +0530

    Normal Changes

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const track = document.querySelector(".crousel-track");
 const slides = document.querySelectorAll(".crousel-item");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
-console.log(track);
 let index = 0;
 
+// image slider
 function updateSlide() {
   track.style.transform = `translateX(-${index * 100}%)`;
 }
@@ -20,6 +20,8 @@ prevBtn.addEventListener("click", () => {
   index = (index - 1 + slides.length) % slides.length;
   updateSlide();
 });
+
+
 /* Toggle Menu */
 hamburger.addEventListener("click", () => {
     menus.classList.toggle("active");
