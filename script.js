0de6bf48e862b55185fab7c39b0ce17a6ad136b4
hyazhar <uddinmdazhar21@gmail.com>
2026-02-16 08:37:17 +0530

    Search Bar Added

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const track = document.querySelector(".crousel-track");
 const slides = document.querySelectorAll(".crousel-item");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
+let searchicon=document.querySelector(".search-container");
+let searchContainer = document.querySelector(".search-bar-container");
+let closeIcon = document.getElementById("close-icon");
 let index = 0;
 // image slider
 function updateSlide() {
@@ -52,5 +55,16 @@ function smoothScroll() {
   window.scrollTo(0, currentScroll);
   requestAnimationFrame(smoothScroll);
 }
-
 smoothScroll();
+// Search Bar Toggle
+searchicon.addEventListener("click", () => {
+    if (searchContainer.style.display === "block") {
+        searchContainer.style.display = "none";
+    } else {
+        searchContainer.style.display = "block";
+    }
+});
+
+closeIcon.addEventListener("click", () => {
+    searchContainer.style.display = "none";
+});
